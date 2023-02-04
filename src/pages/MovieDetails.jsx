905c96c3c72739ec86f288bb5f53bd5a8ea643d3
claytonfortunato <claytonreis10@gmail.com>
2023-02-04 12:33:36 -0300

    feat:page de detalhes finalizado

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Details from "../components/Details";
 
-import MovieCard from "../components/MovieCard";
+import { FaWallet, FaStar } from "react-icons/fa";
+import { BsGraphUp, BsHourglassSplit } from "react-icons/bs";
+import { MdDescription } from "react-icons/md";
 
 const API_IMG = "https://image.tmdb.org/t/p/w500";
 
-const MovieDetails = (image) => {
+import "./MovieDetails.scss";
+
+const MovieDetails = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
 
@@ -26,10 +29,41 @@ const MovieDetails = (image) => {
     <div className="movie-details">
       {movie && (
         <>
-          <MovieCard image={image} title={movie.title} />
+          <h1>{movie.title}</h1>
+          <img src={API_IMG + movie.backdrop_path} alt="" />
           <p className="tagline">{movie.tagline}</p>
-          <p>{movie.runtime} minutos</p>
-          <p>{movie.overview} </p>
+          <div className="details">
+            <div className="budget">
+              <h4>
+                <FaWallet color="94e4eb" size={20} /> Orçamento:
+              </h4>
+              <p>${movie.budget} </p>
+            </div>
+            <div className="revenue">
+              <h4>
+                <BsGraphUp color="94e4eb" size={20} /> Receita:
+              </h4>
+              <p>${movie.revenue}</p>
+            </div>
+            <div className="runtime">
+              <h4>
+                <BsHourglassSplit color="94e4eb" size={20} /> Duração:
+              </h4>
+              <p>{movie.runtime} Minutos</p>
+            </div>
+            <div className="vote">
+              <h4>
+                <FaStar color="94e4eb" size={20} /> Media:
+              </h4>
+              <p>{movie.vote_average}</p>
+            </div>
+          </div>
+          <div className="description">
+            <h4>
+              <MdDescription color="94e4eb" size={20} /> Descrição:
+            </h4>
+            <p>{movie.overview} </p>
+          </div>
         </>
       )}
     </div>
